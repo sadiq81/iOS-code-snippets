2588e8179563f6a3ff00af882a2fed15cb62f94b
Tommy Sadiq Hinrichsen <tommy.lynge@gmail.com>
2017-03-14 08:15:50 +0100

    Create RefreshViewState.swift

---
+++ RefreshView/RefreshViewState.swift
@@ -0,0 +1,13 @@
+//
+// Created by Tommy Sadiq Hinricjhsen on 06/03/2017.
+// Copyright (c) 2017 Eazy IT. All rights reserved.
+//
+
+import Foundation
+
+enum RefreshViewState: Int {
+    case disabled = 0
+    case enabled = 1
+    case willRefresh = 2
+    case refreshing = 3
+}
